44597b5d058e5bdad968ca54b139ad9f78d329a2
Marcellino-cell <fahrifajar379@gmail.com>
2026-09-10 18:53:43 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,229 +1,73 @@
-const CACHE_NAME =
-    "sistem-inventaris-v5-4";
-
+const CACHE_NAME = "sistem-inventaris-v5-6";
 
 const APP_FILES = [
-
     "index.html",
-
     "style.css",
-
     "script.js",
-
     "manifest.json",
-
     "icon/logo 1.jpeg"
-
 ];
 
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.addAll(APP_FILES))
+            .then(() => self.skipWaiting())
+    );
+});
 
-self.addEventListener(
-    "install",
-    function (
-        event
-    ) {
-
-        event.waitUntil(
-
-            caches
-                .open(
-                    CACHE_NAME
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches
+            .keys()
+            .then((keys) =>
+                Promise.all(
+                    keys
+                        .filter((key) => key !== CACHE_NAME)
+                        .map((key) => caches.delete(key))
                 )
-                .then(
-                    function (
-                        cache
-                    ) {
+            )
+            .then(() => self.clients.claim())
+    );
+});
 
-                        return cache.addAll(
-                            APP_FILES
-                        );
-
-                    }
-                )
-                .then(
-                    function () {
-
-                        return self.skipWaiting();
-
-                    }
-                )
-
-        );
-
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET") {
+        return;
     }
-);
 
+    event.respondWith(
+        caches.match(event.request).then((cached) => {
+            if (cached) {
+                return cached;
+            }
 
-self.addEventListener(
-    "activate",
-    function (
-        event
-    ) {
-
-        event.waitUntil(
-
-            caches
-                .keys()
-                .then(
-                    function (
-                        keys
+            return fetch(event.request)
+                .then((response) => {
+                    if (
+                        response &&
+                        response.status === 200 &&
+                        response.type !== "opaque"
                     ) {
+                        const copy = response.clone();
 
-                        return Promise.all(
-
-                            keys
-                                .filter(
-                                    function (
-                                        key
-                                    ) {
-
-                                        return (
-                                            key !==
-                                            CACHE_NAME
-                                        );
-
-                                    }
-                                )
-                                .map(
-                                    function (
-                                        key
-                                    ) {
-
-                                        return caches.delete(
-                                            key
-                                        );
-
-                                    }
-                                )
-
-                        );
-
+                        caches.open(CACHE_NAME).then((cache) => {
+                            cache.put(event.request, copy);
+                        });
                     }
-                )
-                .then(
-                    function () {
 
-                        return self.clients.claim();
-
+                    return response;
+                })
+                .catch(() => {
+                    if (event.request.mode === "navigate") {
+                        return caches.match("index.html");
                     }
-                )
 
-        );
-
-    }
-);
-
-
-self.addEventListener(
-    "fetch",
-    function (
-        event
-    ) {
-
-        if (
-            event.request.method !==
-            "GET"
-        ) {
-
-            return;
-
-        }
-
-
-        event.respondWith(
-
-            caches
-                .match(
-                    event.request
-                )
-                .then(
-                    function (
-                        cached
-                    ) {
-
-                        if (
-                            cached
-                        ) {
-
-                            return cached;
-
-                        }
-
-
-                        return fetch(
-                            event.request
-                        )
-                        .then(
-                            function (
-                                response
-                            ) {
-
-                                if (
-                                    response &&
-                                    response.status ===
-                                    200 &&
-                                    response.type !==
-                                    "opaque"
-                                ) {
-
-                                    const copy =
-                                        response.clone();
-
-
-                                    caches
-                                        .open(
-                                            CACHE_NAME
-                                        )
-                                        .then(
-                                            function (
-                                                cache
-                                            ) {
-
-                                                cache.put(
-                                                    event.request,
-                                                    copy
-                                                );
-
-                                            }
-                                        );
-
-                                }
-
-
-                                return response;
-
-                            }
-                        )
-                        .catch(
-                            function () {
-
-                                if (
-                                    event.request.mode ===
-                                    "navigate"
-                                ) {
-
-                                    return caches.match(
-                                        "index.html"
-                                    );
-
-                                }
-
-
-                                return new Response(
-                                    "",
-                                    {
-                                        status:
-                                            503
-                                    }
-                                );
-
-                            }
-                        );
-
-                    }
-                )
-
-        );
-
-    }
-);
+                    return new Response("", {
+                        status: 503
+                    });
+                });
+        })
+    );
+});
